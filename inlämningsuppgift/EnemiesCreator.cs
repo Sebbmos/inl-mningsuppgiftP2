2f3da38895f939fda63db2f86606a18dea06fbe4
Sebbmos <sebastian.moscosob@gmail.com>
2025-09-18 20:56:13 +0200

    jobbar på fiender

--- inlämningsuppgift/EnemiesCreator.cs
+++ inlämningsuppgift/EnemiesCreator.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System.Security.Cryptography.X509Certificates;
+
 namespace inlämningsuppgift
 {
     public class EnemiesCreator
@@ -14,7 +16,7 @@ namespace inlämningsuppgift
 
 
 
-        public string[] enemies = { "evilRat", "Guard", "Thug", "Skeleton", "The Jailor", "JavaJack" };
+        public string[] Enemies = { "evilRat", "Guard", "Thug", "Skeleton", "The Jailor", "JavaJack" };
 
         public EnemiesCreator(string name, int hp, string damage, int goldReward, string[] enemies)
         {
@@ -22,7 +24,60 @@ namespace inlämningsuppgift
             Hp = hp;
             Damage = damage;
             GoldReward = goldReward;
-            this.enemies = enemies;
+            Enemies = enemies;
+
+           
+
         }
+        public Enemy()
+        {
+            Random rndm = new Random();
+
+            int next = rndm.Next(EnemiesCreator)
+
+                EnemyName = Enemies[i];
+
+
+            switch (Enemies)
+            {
+
+                case "EvilRat":
+                    Hp = 10;
+                    Damage = 8;
+                    GoldReward = 0;
+                    break;
+
+                case "Guard":
+                    Hp = 50;
+                    Damage = 25;
+                    GoldReward = 20;
+                    break;
+                case "Thug":
+                    Hp = 65;
+                    Damage = 17;
+                    GoldReward = 16;
+                    break;
+                case "Skeleton":
+                    Hp = 70;
+                    Damage = 23;
+                    GoldReward = 23;
+                    break;
+                case "The jailor":
+                    Hp = 100;
+                    Damage = 34;
+                    GoldReward = 100;
+                    break;
+                case "JavaJack":
+                    Hp = 1;
+                    Damage = 0;
+                    Goldreward = 100;
+
+
+
+            }
+
+        }
+         
+
     }
 }
